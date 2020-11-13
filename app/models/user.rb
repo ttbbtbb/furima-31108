2be5d7a703d427041672e31ec53ebef7b480efde
ttbbtbb <m86.tabata.masao@gmail.com>
2020-11-14 00:56:26 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :email, uniqueness: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :family_name, presence: true, format: { with: /\A[ァ-ヶー－ぁ-ん一-龥]+\z/ }
+  validates :given_name, presence: true, format: { with: /\A[ァ-ヶー－ぁ-ん一-龥]+\z/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :given_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_date, presence: true
 end
