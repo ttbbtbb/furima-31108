dd42d12345c281db4f9fa5fa2f086ce6bfc93bf5
ttbbtbb <m86.tabata.masao@gmail.com>
2020-11-15 04:54:23 +0900

    Rubocopによる自動修正
    % bundle exec rubocop -a

--- test/controllers/checks_controller_test.rb
+++ test/controllers/checks_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class ChecksControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get checks_index_url
     assert_response :success
   end
-
 end
