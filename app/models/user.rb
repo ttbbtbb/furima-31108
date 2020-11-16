db6d6dc0c4847914caa478901cfcc0ddf18c1dbc
ttbbtbb <m86.tabata.masao@gmail.com>
2020-11-16 15:36:48 +0900

    定数位置微調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  EISU_MIX_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  WIDE_REGEX = /\A[ァ-ヶー－ぁ-ん一-龥]+\z/.freeze
+  WIDE_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+
   validates :email, uniqueness: true
-  EISU_MIX_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates :password, format: { with: EISU_MIX_REGEX }
   with_options presence: true do
     validates :nickname
     validates :birth_date
-    WIDE_REGEX = /\A[ァ-ヶー－ぁ-ん一-龥]+\z/
     validates :family_name, format: { with: WIDE_REGEX }
     validates :given_name, format: { with: WIDE_REGEX }
-    WIDE_KATAKANA_REGEX = /\A[ァ-ヶー－]+\z/
     validates :family_name_kana, format: { with: WIDE_KATAKANA_REGEX }
     validates :given_name_kana, format: { with: WIDE_KATAKANA_REGEX }
   end
