4c76bf74c1ec52d5a365b1399c12dcb65adb3dbb
ttbbtbb <m86.tabata.masao@gmail.com>
2020-11-18 18:52:23 +0900

    RVCの雛形作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,20 @@
 class OrdersController < ApplicationController
+  def new
+    @model = Model.new
+  end
+
+  def create
+    @model = Model.new(model_params)
+    if @model.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def model_params
+    params.require(:model).permit(:column).merge(user_id: current_user.id)
+  end
 end
